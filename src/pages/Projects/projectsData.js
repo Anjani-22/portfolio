c4c8c4b3e2db0f17bc750cfd20c1b51a85a59bec
Anjani-22 <anjaniofficial22@gmail.com>
2024-03-05 08:50:27 +0530

    untracked deleted files

---
+++ src/pages/Projects/projectsData.js
@@ -0,0 +1,75 @@
+const projects = [
+  {
+    id: 10,
+    title: "Navigating Tabs in react",
+    image: "./ProjectImages/react-tabs.jpg",
+    link: "https://heroic-arithmetic-1a1664.netlify.app",
+  },
+  {
+    id: 11,
+    title: "Movies Rating",
+    image: "./ProjectImages/movies-rating.jpg",
+    link: "https://grand-pie-586ece.netlify.app/",
+  },
+  {
+    id: 9,
+    title: "React Quiz - using useReducer",
+    image: "./ProjectImages/react-quiz.jpg",
+    link: "https://comforting-sherbet-1505cf.netlify.app",
+  },
+  {
+    id: 8,
+    title: "Travel check list",
+    image: "./ProjectImages/travel-list.jpg",
+    link: "https://effulgent-clafoutis-833654.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "Orchid Arts",
+    image: "./ProjectImages/orchids.jpg",
+    description: "A web application to sell the creative art using flower   ",
+    link: "https://659fe3061933d70907878d65--scintillating-genie-d73197.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "Age Calculator in Vijay Dinanath Style",
+    image: "./ProjectImages/age-calculator.jpg",
+    link: "https://65a191d8ffa330357f1f57e5--glowing-crumble-7fad31.netlify.app/",
+  },
+  {
+    id: 7,
+    title: "Collection of mini projects",
+    image: "./ProjectImages/miniProject.jpg",
+    link: "https://65b939589611e31e1610b0bb--subtle-gaufre-1f1011.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "BookList",
+    image: "./ProjectImages/BookListSiteImg.jpg",
+    description: "Add, remove and check year & author of your books ",
+    link: "https://addremovebook.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "First Bio",
+    image: "./ProjectImages/OldBioSite.jpg",
+    description: "Old Bio Site",
+    link: "https://smecanjani.github.io/My-first-Site/",
+  },
+  {
+    id: 3,
+    title: "Old Doctors Site",
+    image: "./ProjectImages/OldDoctorsSite.jpg",
+    description: "Old Doctors Site",
+    link: "https://smecanjani.github.io/doctor_font_end_only/",
+  },
+  {
+    id: 4,
+    title: "Tic tac Toe(Pesto Assignment)",
+    image: "./ProjectImages/tictactoe.jpg",
+    description: "TicTacToe (Pesto Assignment",
+    link: "https://aesthetic-pasca-1d4d3b.netlify.app",
+  },
+];
+
+export default projects;
